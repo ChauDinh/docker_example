e91f6b9206358377a4cc56c36d374ab97684e35a
ChauDinh <chaudvb95uc@gmail.com>
2018-12-31 15:13:01 +0700

    Server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require("./src/index");
+const mongoose = require("mongoose");
+
+// connect to mongo
+mongoose.connect("mongodb://0.0.0.0:27017/docker-node-mongo", { useNewUrlParser: true })
+ .then(() => console.log("Connected to MongoDB"))
+ .catch(err => console.error(err));
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+
+process.on("unhandledRejection", e => {
+ console.log(e.message);
+ process.exit(1);
+});
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const Item = require("../models/Item");
+
+app.get("/", (req, res) => {
+ Item.find()
+  .then(items => res.render("index", { items }))
+  .catch(err => res.status(404).json({ msg: "No items found" }));
+});
+
+app.post("/item/add", (req, res) => {
+ const newItem = new Item({
+  name: req.body.name
+ });
+
+ newItem.save().then(item => res.redirect("/"));
+});
+
+module.exports = app;
